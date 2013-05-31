418fda14f6d7bb5f222049e657b5a2e621a2ff6d
Edward <ed@htmelvis.com>
2013-05-31 11:32:53 -0400

    Added Assets Folder and Plugin Comments

---
+++ pdf-organizer.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+	Plugin Name: PDF Organizer
+	Plugin URI: http://myriadcore.com
+	Description: PDF Organizer Plugin for Organizing Media Attachments
+	Version: 1.0
+	Author: Edward Wieczorek
+	Author URI: http://myriadcore.com
+*/
+?>
